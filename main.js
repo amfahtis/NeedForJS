e43c2c439cddd9462be3e04cb37543d72a0d7732
Saidakbar <30874817+amfahtis@users.noreply.github.com>
2021-06-24 20:42:09 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,8 @@ console.log(getQuantityElements(200));
 
 function startGame(){
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+   
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -50,7 +51,11 @@ function startGame(){
     }
 
     setting.start = true;
+    setting.score = 0;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -60,6 +65,8 @@ function startGame(){
 function playGame(){
     
     if (setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE<br>' + setting.score;
         moveRoad();
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0){
@@ -109,7 +116,21 @@ function moveRoad(){
 
 function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if(carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            console.warn('Crash');
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight;
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y > document.documentElement.clientHeight) {
